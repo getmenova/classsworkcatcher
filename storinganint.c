99f75c40ae508c546e794c5624ffc3e220fd7623
Michael A Nowiszewski <micski@gmail.com>
2018-04-02 16:15:56 +0000

    x

--- storinganint.c
+++ storinganint.c
@@ -5,6 +5,7 @@ int main(void) //not intended to be run as a standalone
 {
 printf ("Hello there\n");
     int integer = 5 + 8;
+    // 101 + 1000 = 1101
     printf("Here's my number: %i\n", integer);
 
 
