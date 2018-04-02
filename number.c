d9d63e7f344446926f36e704738a7f930f6960b4
Michael A Nowiszewski <micski@gmail.com>
2018-04-02 16:06:33 +0000

    updated addition

--- number.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h> // header file
-
-int main(void) //not intended to be run as a standalone
-{
-printf ("Hello there\n");
-}
-
-//this does this -  take the time to google it
--- storinganint.c
+++ storinganint.c
@@ -1,16 +1,15 @@
 #include<stdio.h> // header file
 
-int stuff()
-{
-
-    printf("stuff\n");
-    return 1;
-}
 int main(void) //not intended to be run as a standalone
+
 {
-stuff();
-stuff("hello");
 printf ("Hello there\n");
+    int integer = 5 + 8;
+    printf("Here's my number: %i\n", integer);
+
+
 }
+
+
 
 //this does this -  take the time to google it
