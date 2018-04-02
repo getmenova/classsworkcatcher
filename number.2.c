7d79d57bba0e7f0d1a622dfbc729823ad8d2ba3d
Michael A Nowiszewski <micski@gmail.com>
2018-04-02 15:43:06 +0000

    throw error / void demo

---
+++ number.2.c
@@ -0,0 +1,16 @@
+#include<stdio.h> // header file
+
+int stuff()
+{
+
+    printf("stuff\n");
+    return 1;
+}
+int main(void) //not intended to be run as a standalone
+{
+stuff();
+stuff("hello");
+printf ("Hello there\n");
+}
+
+//this does this -  take the time to google it
