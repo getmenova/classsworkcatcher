aae06facbcdb6b29aca0b9a8ed7448996817542b
Michael A Nowiszewski <micski@gmail.com>
2018-04-05 01:23:28 +0000

    update challenge prompt for class assignment

---
+++ 04.04.2018/week 1 notes and test/04.02.2018/number.3.c
@@ -0,0 +1,25 @@
+#include<stdio.h> // header file
+#include<cs50.h>
+int stuff()
+{
+
+    printf("stuff\n");
+    return 1;
+
+    }
+/**/
+    int stuffs(char* stuff)
+    {
+        printf("%\n", stuff);
+        return 1;
+
+    } /**/ 
+int main(void) //not intended to be run as a standalone
+{
+stuff();
+stuff("hello");
+stuffs
+printf ("Hello there\n");
+}
+
+//this does this -  take the time to google it
---
+++ 04.04.2018/week 1 notes and test/04.02.2018/xyz.c
@@ -0,0 +1,18 @@
+#include<stdio.h> // header file
+#include<cs50.h>
+#include<math.h> //
+int main(void) //not intended to be run as a standalone
+
+{
+printf ("Hello there\n");
+    int integer = 5 + 8;
+    // 101 + 1000 = 1101
+    printf("Here's my number: %i\n", integer);
+    float flt = 5.3 + 4.83;
+    printf("Here's my float%.20f\n", flt);
+
+}
+
+
+
+//this does this -  take the time to google it
