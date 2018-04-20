85575f208773c2761641d5b36c4775481a7b6936
Michael A Nowiszewski <micski@gmail.com>
2018-04-20 22:01:34 +0000

    playing with pigeonholes

---
+++ 4.14.018/demo/strings/string.3.c
@@ -0,0 +1,22 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+int main(int argc, string argv[])
+
+{
+    if (argc !=2){
+
+        printf("Usage: strings \"phrase\"\n")
+    }
+    return 0;
+
+    printf("%i\n", argc);
+    int length  = strlen(argv[1]);
+    printf("%i\t%s\n", length, argv[0]);
+
+}
+
---
+++ 4.14.018/demo/strings/string.c
@@ -0,0 +1,29 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (int argc, string argv[])
+
+{
+    if (argc !=2)
+    {
+
+        printf("Usage: strings \"phrase\"\n");
+      exit(1);
+    }
+
+    //printf("%i\n", argc);
+    //int length  = strlen(argv[1]);
+    //printf("%i\t%s\n", length, argv[0]);
+
+}
+
+for (int i = 0, length = strlen(argv[1]); i < length; i++);
+        if (argv[1][i] != ' ')
+{
+    printf("the ascii value at pos %i: c%\n", i, argv[1][i]);
+        }
+    }
+}
+
