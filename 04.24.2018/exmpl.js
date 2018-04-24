e6f29ee3e5a639d0a1c976597538513f782243c9
Michael A Nowiszewski <micski@gmail.com>
2018-04-24 15:15:26 +0000

    class notes checkpoint

---
+++ 04.24.2018/exmpl.js
@@ -0,0 +1,17 @@
+var addTwo (x,y) {
+    var addTwo = function(x,y)
+    x=2;
+    return x+y;
+}
+
+    var num = 5
+
+    var number =2;
+
+    console.log(num)
+
+
+console.log(addTwo(num,number))
+
+
+// showed data changing
